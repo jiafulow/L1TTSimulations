08abf97c50a1c75ac51ab46a54f08f6867dd460e
Jia Fu Low <jia.fu.low@cern.ch>
2016-05-06 20:08:11 -0400

    Add GeometryTools

---
+++ GeometryTools/interface/ModuleIdFunctor.h
@@ -0,0 +1,14 @@
+#ifndef GeometryTools_ModuleIdFunctor_h_
+#define GeometryTools_ModuleIdFunctor_h_
+
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/SiPixelDetId/interface/PixelSubdetector.h"
+#include "DataFormats/SiPixelDetId/interface/PXBDetId.h"
+#include "DataFormats/SiPixelDetId/interface/PXFDetId.h"
+
+class ModuleIdFunctor {
+  public:
+    uint32_t operator() (const DetId& id) const;
+};
+
+#endif
