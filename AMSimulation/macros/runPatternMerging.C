fe649962552bdbf3a9dcf97d6189f017d4e14163
Jia Fu Low <jia.fu.low@cern.ch>
2016-11-14 17:49:49 -0500

    Update runPatternMerging.C

--- AMSimulation/macros/runPatternMerging.C
+++ AMSimulation/macros/runPatternMerging.C
@@ -2,12 +2,17 @@
 
 
 void runPatternMerging() {
-  TString patternBankFileName     = "../dataFiles/patternBank_tt27_sf1L0x2_nz8_pt3_300M.root";
-  TString tempPatternBankFileName = "../dataFiles/m8_patternBank_tt27_sf1L0x2_nz8_pt3_300M.root";
-  //unsigned deltaN                 = 36000;
+  //TString patternBankFileName     = "../dataFiles/patternBank_tt27_sf1L0x2_nz8_pt3_300M.root";
+  //TString tempPatternBankFileName = "../dataFiles/m8_patternBank_tt27_sf1L0x2_nz8_pt3_300M.root";
+  ////unsigned deltaN                 = 36000;
+  //unsigned deltaN                 = 60000;
+  //float targetCoverage            = 1.00;
+  ////float targetCoverage            = 1.95;
+
+  TString patternBankFileName     = "../dataFiles/patternBank_oc_tt25_sf1_nz8_pt3_400M.root";
+  TString tempPatternBankFileName = "../dataFiles/m8_patternBank_oc_tt25_sf1_nz8_pt3_400M.root";
   unsigned deltaN                 = 60000;
   float targetCoverage            = 1.00;
-  //float targetCoverage            = 1.95;
 
   PatternMerging pm;
   pm.mergePatterns(patternBankFileName, tempPatternBankFileName, deltaN, targetCoverage);
